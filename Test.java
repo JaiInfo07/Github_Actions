05902231104bfc53bfa00e58ab8b8f5fe0f95154
JaiInfo07 <kavivignesh07@gmail.com>
2024-08-26 19:28:05 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-Class Test{
-  public static void main(String[] args){
-    System.out.println("Hello, World !");
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
   }
 }
