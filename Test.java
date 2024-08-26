0120f5a0a27275ef108cd43b6d9bfa27db892451
JaiInfo07 <kavivignesh07@gmail.com>
2024-08-26 19:47:17 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,19 +1,5 @@
-/**
- * The entry point of the application.
- */
-public class Test {
-
-  // Private constructor to prevent instantiation
-  private Test() {
-    // private constructor to prevent instantiation
-  }
-
-  /**
-   * The main method to start the application.
-   *
-   * @param args Command-line arguments
-   */
-  public static void main(final String[] args) {
+class Test {
+  public static void main(String[] args) {
     System.out.println("Hello, World!");
   }
 }
